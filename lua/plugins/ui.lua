06326331a314b967ec556dbd13a7f4ea83ff5f29
Victor Salazar Lopez <salazarlopezvictor@outlook.com>
2026-07-14 17:48:50 -0600

    refactor: move mini.icons plugin to the ui plugins file

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,29 @@
 ---@type LazySpec[]
 return {
   {
+    "nvim-mini/mini.icons",
+    version = false,
+    specs = {
+      { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+    },
+    opts = {
+      file = {
+        [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+        ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+      },
+      filetype = {
+        dotenv = { glyph = "", hl = "MiniIconsYellow" },
+      },
+    },
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+  },
+
+  {
     "folke/snacks.nvim",
     lazy = false,
     priority = 1000,
