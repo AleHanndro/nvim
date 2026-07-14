3b7462f5b353cdca02cce8031cb8db12f3155868
Victor Salazar Lopez <salazarlopezvictor@outlook.com>
2026-07-14 17:48:49 -0600

    refactor: move blink and luasnip config to their own plugin file

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -10,7 +10,24 @@ return {
         opts = { history = true, updateevents = "TextChanged,TextChangedI" },
         config = function(_, opts)
           require("luasnip").config.set_config(opts)
-          require "configs.luasnip"
+
+          require("luasnip.loaders.from_vscode").lazy_load { exclude = vim.g.vscode_snippets_exclude or {} }
+          require("luasnip.loaders.from_vscode").lazy_load { paths = vim.g.vscode_snippets_path or "" }
+
+          require("luasnip.loaders.from_snipmate").load()
+          require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snippets_path or "" }
+
+          -- fix luasnip #258
+          vim.api.nvim_create_autocmd("InsertLeave", {
+            callback = function()
+              if
+                require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+                and not require("luasnip").session.jump_active
+              then
+                require("luasnip").unlink_current()
+              end
+            end,
+          })
         end,
       },
       {
@@ -34,6 +51,39 @@ return {
       },
     },
     event = "InsertEnter",
-    opts = function() return require "configs.blink" end,
+    opts = function()
+      ---@module 'blink-cmp'
+      ---@type blink.cmp.Config
+      return {
+        snippets = { preset = "luasnip" },
+        cmdline = { enabled = false },
+        appearance = { nerd_font_variant = "normal" },
+        fuzzy = { implementation = "prefer_rust" },
+        sources = {
+          default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+          providers = {
+            lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", score_offset = 100 },
+          },
+        },
+
+        keymap = {
+          preset = "default",
+          ["<CR>"] = { "accept", "fallback" },
+          ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+          ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+          ["<C-k>"] = {}, -- C-k is mapped to <Up> on insert mode
+          ["<C-s>"] = { "show_signature", "hide_signature", "fallback" },
+        },
+
+        completion = {
+          documentation = { auto_show = false, auto_show_delay_ms = 500 },
+          list = {
+            selection = { preselect = false, auto_insert = true },
+          },
+        },
+
+        signature = { enabled = true },
+      }
+    end,
   },
 }
