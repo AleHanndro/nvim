dd1e7b99c070ef66780c3ec4a021e3cbee4f5303
Victor Salazar Lopez <salazarlopezvictor@outlook.com>
2026-07-14 17:48:50 -0600

    refactor: move neovim config to configs folder

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 require "configs.lazy"
 
-require "options"
-require "autocmds"
-vim.schedule(function() require "keymaps" end)
+require "configs.options"
+require "configs.autocmds"
+vim.schedule(function() require "configs.keymaps" end)
 
 -- vim: ts=2 sts=2 sw=2 et
