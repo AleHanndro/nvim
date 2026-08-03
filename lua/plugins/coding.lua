df9362ca02abd332082023e29fb70e5abb0b10ac
Victor Salazar Lopez <salazarlopezvictor@outlook.com>
2026-08-03 15:07:49 -0600

    feat(plugins): add mini.surround configuration and keymaps
    Signed-off-by: Victor Salazar Lopez <salazarlopezvictor@outlook.com>

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -9,6 +9,35 @@ return {
   },
 
   {
+    "nvim-mini/mini.surround",
+    keys = function(self)
+      local opts = self.opts or {}
+      local m = opts.mappings or {}
+
+      return {
+        { m.add, desc = "Add Surrounding", mode = { "n", "x" } },
+        { m.delete, desc = "Delete Surrounding" },
+        { m.find, desc = "Find Right Surrounding" },
+        { m.find_left, desc = "Find Left Surrounding" },
+        { m.highlight, desc = "Highlight Surrounding" },
+        { m.replace, desc = "Replace Surrounding" },
+        { m.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+      }
+    end,
+    opts = {
+      mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+      },
+    },
+  },
+
+  {
     "folke/lazydev.nvim",
     ft = "lua",
     opts = {
