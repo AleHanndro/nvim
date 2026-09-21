f56300356246de2f523c57e0de539911b9d9ae58
Victor Salazar Lopez <salazarlopezvictor@outlook.com>
2026-09-21 15:58:07 -0600

    feat(keymaps): add Snacks picker keymaps
    Signed-off-by: Victor Salazar Lopez <salazarlopezvictor@outlook.com>

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -10,7 +10,7 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 
 map("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
 map("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
-map("n", "<C-q>", function() Snacks.bufdelete.delete { wipe = true, force = false } end, { desc = "Delete buffer" })
+map("n", "<C-q>", function() Snacks.bufdelete.delete() end, { desc = "Delete buffer" })
 
 map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
@@ -18,5 +18,22 @@ map("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 map("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 map("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 map("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Find
+map("n", "<leader>fb", function() Snacks.picker.buffers() end, { desc = "Buffers" })
+map("n", "<leader>ff", function() Snacks.picker.files() end, { desc = "Find files" })
+-- Search
+map("n", "<leader>sb", function() Snacks.picker.lines() end, { desc = "Search Buffer Lines" })
+map("n", "<leader>sB", function() Snacks.picker.grep_buffers() end, { desc = "Search Open Buffers" })
+map("n", "<leader>sd", function() Snacks.picker.diagnostics() end, { desc = "Diagnostics" })
+map("n", "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, { desc = "Buffer Diagnostics" })
+map("n", "<leader>sg", function() Snacks.picker.grep() end, { desc = "Search Workspace" })
+map("n", "<leader>sh", function() Snacks.picker.help() end, { desc = "Search Help Pages" })
+map("n", "<leader>sk", function() Snacks.picker.keymaps() end, { desc = "Search Keymaps" })
+map("n", "<leader>sq", function() Snacks.picker.qflist() end, { desc = "Quickfix List" })
+map({ "n", "x" }, "<leader>sw", function() Snacks.picker.grep_word() end, { desc = "Search visual selection or word" })
+
+-- Explorer
+map("n", "<C-n>", function() Snacks.picker.explorer() end, { desc = "File Explorer" })
 
 -- vim: ts=2 sts=2 sw=2 et
