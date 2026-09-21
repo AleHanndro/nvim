5b6658231237f36c977b7d5eac416e750484fd3a
Victor Salazar Lopez <salazarlopezvictor@outlook.com>
2026-09-21 16:08:31 -0600

    refactor(autocmds): clenaup LspAttch autocommands and add augroups
    Without `augroup`s, each autocmd gets fired again on buffer changes,
    adding an augroup prevents this behavior.
    
    This also adds a custom User Event (inspired from NvChad) which allows
    plugins dependant on the FileType event to be deferred and loaded after
    first paint, it's like VeryLazy but we can load nvim-lspconfig here. The
    only plugin that needs a workaround to work with this is the
    mason-tool-installer plugin.
    
    Signed-off-by: Victor Salazar Lopez <salazarlopezvictor@outlook.com>

--- lua/configs/autocmds.lua
+++ lua/configs/autocmds.lua
@@ -1,67 +1,91 @@
-local autocmd = vim.api.nvim_create_autocmd
+local api = vim.api
+local map = vim.keymap.set
 
-autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("custom-lsp-attach", { clear = true }),
+api.nvim_create_autocmd("LspAttach", {
+  group = api.nvim_create_augroup("custom-lsp-attach", { clear = true }),
   callback = function(args)
-    local map = function(keys, func, desc, mode)
-      mode = mode or "n"
-      vim.keymap.set(mode, keys, func, { buffer = args.buf, desc = "LSP: " .. desc })
-    end
+    local function opts(desc) return { buffer = args.buf, desc = "LSP: " .. desc } end
 
-    map("grn", vim.lsp.buf.rename, "rename")
-    map("gra", vim.lsp.buf.code_action, "goto code action", { "n", "x" })
-    map("grD", vim.lsp.buf.declaration, "goto declaration")
-
-    map("gri", "<cmd>FzfLua lsp_implementations<CR>", "goto implementation")
-    map("grr", "<cmd>FzfLua lsp_references<CR>", "goto references")
-    map("grt", "<cmd>FzfLua lsp_typedefs<CR>", "goto type definition")
-    map("gO", "<cmd>FzfLua lsp_document_symbols<CR>", "open document symbols")
-    map("gW", "<cmd>FzfLua lsp_workspace_symbols<CR>", "open workspace symbols")
+    map("n", "gd", function() Snacks.picker.lsp_definitions() end, opts "Goto Definition")
+    map("n", "gD", function() Snacks.picker.lsp_declarations() end, opts "Goto Declaration")
+    map("n", "gri", function() Snacks.picker.lsp_implementations() end, opts "Goto Implementation")
+    map("n", "grr", function() Snacks.picker.lsp_references() end, opts "References")
+    map("n", "grt", function() Snacks.picker.lsp_type_definitions() end, opts "Goto Type Definition")
+    map("n", "<leader>ss", function() Snacks.picker.lsp_symbols() end, opts "Symbols")
+    map("n", "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, opts "Workspace Symbols")
 
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     if client and client:supports_method("textDocument/documentHighlight", args.buf) then
-      local highlight_augroup = vim.api.nvim_create_augroup("custom-lsp-highlight", { clear = false })
-      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+      local highlight_augroup = api.nvim_create_augroup("custom-lsp-highlight", { clear = false })
+      api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
         buffer = args.buf,
         group = highlight_augroup,
         callback = vim.lsp.buf.document_highlight,
       })
 
-      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+      api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
         buffer = args.buf,
         group = highlight_augroup,
         callback = vim.lsp.buf.clear_references,
       })
 
-      vim.api.nvim_create_autocmd("LspDetach", {
-        group = vim.api.nvim_create_augroup("custom-lsp-detach", { clear = true }),
+      api.nvim_create_autocmd("LspDetach", {
+        group = api.nvim_create_augroup("custom-lsp-detach", { clear = true }),
         callback = function(args2)
           vim.lsp.buf.clear_references()
-          vim.api.nvim_clear_autocmds { group = "custom-lsp-highlight", buffer = args2.buf }
+          api.nvim_clear_autocmds { group = "custom-lsp-highlight", buffer = args2.buf }
         end,
       })
     end
 
     if client and client:supports_method("textDocument/inlayHint", args.buf) then
       map(
+        "n",
         "<leader>th",
         function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = args.buf }) end,
-        "Toggle Inlay Hints"
+        opts "Toggle Inlay hints"
       )
     end
   end,
 })
 
-autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
-  group = vim.api.nvim_create_augroup("custom-lint", { clear = true }),
+api.nvim_create_autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
+  desc = "Emit User FilePost after UIEnter with a file buffer",
+  callback = function(args)
+    if args.event == "UIEnter" then vim.g.ui_entered = true end
+
+    if not vim.g.ui_entered then return end
+    if not api.nvim_buf_is_valid(args.buf) then return end
+    if api.nvim_buf_get_name(args.buf) == "" or vim.bo[args.buf].buftype ~= "" then return end
+
+    api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
+
+    vim.schedule(function()
+      if not api.nvim_buf_is_valid(args.buf) then return end
+
+      local ft = vim.bo[args.buf].filetype
+      if ft ~= "" then
+        api.nvim_buf_call(
+          args.buf,
+          function() api.nvim_exec_autocmds("FileType", { pattern = ft, modeline = false }) end
+        )
+      end
+    end)
+
+    return true
+  end,
+})
+
+api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+  group = api.nvim_create_augroup("custom-lint", { clear = true }),
   callback = function()
     if vim.bo.modifiable then require("lint").try_lint() end
   end,
 })
 
 -- highlight when yanking (copying) text
-autocmd("TextYankPost", {
+api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  group = api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
